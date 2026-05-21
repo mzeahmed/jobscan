047c269689d204ce87514f11d7a435b1486a5bc3
Ahmed Mze <houdjiva@gmail.com>
2026-05-21 12:52:00 +0200

    feat: vue HTML paginée de listing des offres

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Repository\JobRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class JobController extends AbstractController
 {
