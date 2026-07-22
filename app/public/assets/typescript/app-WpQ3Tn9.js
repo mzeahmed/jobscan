469a40ba4d03b7ba4128e15d22351fabdd5971f6
Ahmed Mze <houdjiva@gmail.com>
2026-07-22 23:58:30 +0200

    v

--- app/public/assets/typescript/app-WpQ3Tn9.js
+++
@@ -1,4 +0,0 @@
-// Point d'entrée TypeScript principal
-// Ce fichier sera compilé automatiquement en app.js par sensiolabs/typescript-bundle
-var greeting = "Jobscan TypeScript charg\xe9 ✅";
-console.log(greeting);
